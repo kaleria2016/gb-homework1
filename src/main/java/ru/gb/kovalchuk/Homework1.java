0f79bc27811cd701bb64cea54701789d1dd63ddd
kaleria2016 <kaleria2016@icloud.com>
2021-11-04 03:59:13 +0700

    first task done

---
+++ src/main/java/ru/gb/kovalchuk/Homework1.java
@@ -0,0 +1,14 @@
+package ru.gb.kovalchuk;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        printThreeWords();
+
+    }
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Apple");
+        System.out.println("Banana");
+
+    }
+}
